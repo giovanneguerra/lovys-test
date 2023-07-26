2c040652ff54412fdb76108757ea55ccd32b16cd
Giovanne Guerra <giovanneguerra@hotmail.com>
2023-07-26 07:45:43 +0100

    feature: Implement guards

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,14 +6,16 @@ import { FavoritesComponent } from './pages/favorites/favorites.component';
 import { TvSeriesComponent } from './pages/tv-series/tv-series.component';
 import { LoginComponent } from './pages/login/login.component';
 import { SignUpComponent } from './pages/sign-up/sign-up.component';
+import { AuthGuard } from './core/guards/auth.guard';
+import { SignGuard } from './core/guards/sign.guard';
 
 const routes: Routes = [
-  { path: 'home', component: HomeComponent },
-  { path: 'movies', component: MoviesComponent },
-  { path: 'favorites', component: FavoritesComponent },
-  { path: 'tv-series', component: TvSeriesComponent },
-  { path: 'login', component: LoginComponent },
-  { path: 'sign-up', component: SignUpComponent },
+  { path: 'home', component: HomeComponent, canActivate: [AuthGuard] },
+  { path: 'movies', component: MoviesComponent, canActivate: [AuthGuard] },
+  { path: 'favorites', component: FavoritesComponent, canActivate: [AuthGuard] },
+  { path: 'tv-series', component: TvSeriesComponent, canActivate: [AuthGuard] },
+  { path: 'login', component: LoginComponent, canActivate: [SignGuard] },
+  { path: 'sign-up', component: SignUpComponent, canActivate: [SignGuard] },
   { path: '**', redirectTo: 'home' }
 ];
 
---
+++ src/app/core/guards/sign.guard.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import {
+  ActivatedRouteSnapshot,
+  RouterStateSnapshot,
+  Router,
+  UrlTree,
+} from '@angular/router';
+import { AuthService } from '../services/auth.service';
+import { Observable, map, take } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class SignGuard {
+  constructor(private authService: AuthService, private router: Router) {}
+
+  canActivate(next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+      return this.authService.isUserLoggedIn().pipe(
+        take(1),
+        map(loggedIn => {
+          return !loggedIn ? true : this.router.createUrlTree(['/']);
+      }));
+  }
+
+};
