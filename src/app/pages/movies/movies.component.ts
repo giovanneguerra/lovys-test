94464005d3e5b675ec2336cc465954175d606418
Giovanne Guerra <giovanneguerra@hotmail.com>
2023-07-27 12:44:23 +0100

    feature: Implement basic requests

--- src/app/pages/movies/movies.component.ts
+++ src/app/pages/movies/movies.component.ts
@@ -1,8 +1,21 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { MediaService } from 'src/app/core/services/media.service';
 
 @Component({
   selector: 'app-movies',
   templateUrl: './movies.component.html',
   styleUrls: ['./movies.component.scss']
 })
-export class MoviesComponent {}
+export class MoviesComponent {
+  movieGenre$: Observable<any[]>;
+  movieListByGenre$: Observable<any[]>;
+  movieDetail$: Observable<any[]>;
+
+  constructor(private mediaService: MediaService) {
+    this.movieGenre$ = this.mediaService.getMovieGenres();
+    this.movieListByGenre$ = this.mediaService.getMovieListByGenre(27);
+    this.movieDetail$ = this.mediaService.getMovieDetail(892);
+  }
+
+}
