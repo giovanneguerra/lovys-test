a6d66e46571bc608f89127554a8f954563a5d25e
Giovanne Guerra <giovanneguerra@hotmail.com>
2023-08-19 12:41:12 +0100

    refactor: Remove tv-series component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,6 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { MoviesComponent } from './pages/movies/movies.component';
 import { FavoritesComponent } from './pages/favorites/favorites.component';
-import { TvSeriesComponent } from './pages/tv-series/tv-series.component';
 import { AuthGuard } from './core/guards/auth.guard';
 import { SignGuard } from './core/guards/sign.guard';
 import { AuthComponent } from './pages/auth/auth.component';
@@ -12,16 +11,23 @@ import { MovieDetailComponent } from './pages/movie-detail/movie-detail.componen
 const routes: Routes = [
   { path: 'home', component: HomeComponent, canActivate: [AuthGuard] },
   { path: 'movies', component: MoviesComponent, canActivate: [AuthGuard] },
-  { path: 'movie-detail/:id', component: MovieDetailComponent, canActivate: [AuthGuard] },
-  { path: 'favorites', component: FavoritesComponent, canActivate: [AuthGuard] },
-  { path: 'tv-series', component: TvSeriesComponent, canActivate: [AuthGuard] },
+  {
+    path: 'movie-detail/:id',
+    component: MovieDetailComponent,
+    canActivate: [AuthGuard],
+  },
+  {
+    path: 'favorites',
+    component: FavoritesComponent,
+    canActivate: [AuthGuard],
+  },
   { path: 'login', component: AuthComponent, canActivate: [SignGuard] },
   { path: 'sign-up', component: AuthComponent, canActivate: [SignGuard] },
-  { path: '**', redirectTo: 'home' }
+  { path: '**', redirectTo: 'home' },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
