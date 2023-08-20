61a4c9a257a336ae9bb10fd66e518911eb06734e
Giovanne Guerra <giovanneguerra@hotmail.com>
2023-08-20 08:33:22 +0100

    feature: Create displayImage pipe

---
+++ src/app/shared/pipes/display-image.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { DisplayImagePipe } from './display-image.pipe';
+
+describe('DisplayImagePipe', () => {
+  it('create an instance', () => {
+    const pipe = new DisplayImagePipe();
+    expect(pipe).toBeTruthy();
+  });
+});
