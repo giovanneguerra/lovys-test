ac35bd482db0cf32367f4adabfbc3576d2a3a9eb
Giovanne Guerra <giovanneguerra@hotmail.com>
2023-08-20 10:00:28 +0100

    refactor: Improve menu UI

--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -1,29 +1,44 @@
 import { Component, OnInit, inject } from '@angular/core';
 import { AuthService } from 'src/app/core/services/auth.service';
-import { RouterLink } from '@angular/router';
-import { NgIf } from '@angular/common';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+import { AsyncPipe, JsonPipe, NgIf } from '@angular/common';
 import { FlexModule } from '@angular/flex-layout/flex';
 import { MatIconModule } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
 import { ExtendedModule } from '@angular/flex-layout/extended';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { Observable } from 'rxjs';
 
 @Component({
-    selector: 'moma-menu',
-    templateUrl: './menu.component.html',
-    styleUrls: ['./menu.component.scss'],
-    standalone: true,
-    imports: [MatToolbarModule, MatButtonModule, ExtendedModule, MatMenuModule, MatIconModule, FlexModule, NgIf, RouterLink]
+  selector: 'moma-menu',
+  templateUrl: './menu.component.html',
+  styleUrls: ['./menu.component.scss'],
+  standalone: true,
+  imports: [
+    MatToolbarModule,
+    MatButtonModule,
+    ExtendedModule,
+    MatMenuModule,
+    MatIconModule,
+    FlexModule,
+    NgIf,
+    RouterLink,
+    AsyncPipe,
+    MatIconModule,
+    RouterLinkActive,
+  ],
 })
-export class MenuComponent implements OnInit{
+export class MenuComponent implements OnInit {
   authService = inject(AuthService);
   isLoggedIn = false;
+  userEmail: Observable<string>;
 
   ngOnInit(): void {
-    this.authService.isUserLoggedIn().subscribe(loggedIn => {
+    this.authService.isUserLoggedIn().subscribe((loggedIn) => {
       this.isLoggedIn = loggedIn;
     });
+    this.userEmail = this.authService.getUserEmail();
   }
 
   logout() {
